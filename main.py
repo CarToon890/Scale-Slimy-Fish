208636a04f14bccd2d2f2191c4e4c919f3946903
Surachat <surachat18525@gmail.com>
2026-08-24 02:48:54 +0700

    Switch codebase back to commit c9a4af9 (1ms Timer, GUI throttling, and Sinking loop grab optimization)

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ import numpy as np
 from PIL import Image, ImageTk
 from bot_core import FishingBot, BotState, InputSimulator
 
-# Enable DPI Awareness at startup
+# Enable DPI Awareness & 1ms Timer Resolution at startup
 if sys.platform == "win32":
     try:
         ctypes.windll.shcore.SetProcessDpiAwareness(2)
@@ -35,6 +35,10 @@ if sys.platform == "win32":
             ctypes.windll.user32.SetProcessDPIAware()
         except Exception:
             pass
+    try:
+        ctypes.windll.winmm.timeBeginPeriod(1)
+    except Exception:
+        pass
     try:
         sys.stdout.reconfigure(encoding="utf-8")
     except Exception:
@@ -899,6 +903,11 @@ class FishingBotApp:
             keyboard.unhook_all_hotkeys()
         except:
             pass
+        if sys.platform == "win32":
+            try:
+                ctypes.windll.winmm.timeEndPeriod(1)
+            except:
+                pass
         self.root.destroy()
 
 
